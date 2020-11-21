163c9327182ba991631528a8258ac2a43aa76e49
Roberto Guerra <uris77@gmail.com>
2020-11-21 15:23:36 -0600

    initial commit

---
+++ cmd/server/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "moh.gov.bz/hecopab/reporting/internal/server"
+
+func main() {
+	server.NewServer()
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module moh.gov.bz/hecopab/reporting
+
+go 1.15
+
+require github.com/gorilla/mux v1.8.0
